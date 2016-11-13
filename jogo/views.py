1b1af7a4cd64500b751ba8c7a04ffde3bdda3027
beatriz-soares <bs.souza1998@gmail.com>
2016-11-13 17:16:26 -0300

    pagina de escolher o nivel

--- jogo/views.py
+++ jogo/views.py
@@ -1,17 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
 # Create your views here.from django.http import HttpResponse
+
 
 def index(request):
     return render(request, "jogo/menu.html")
 
+
 def instrucoes(request):
     return render(request, "jogo/instrucoes.html")
 
+
 def jogo(request):
-    return render(request, "jogo/jogo.html")
-
-
-
-
+    return render(request, "jogo/escolher_nivel.html")
