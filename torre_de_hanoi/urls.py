2bf82d0c153130ae6b5adb9560e8135a8f52bab2
Diego Lima <ddddiegolima@gmail.com>
2016-11-13 17:06:11 -0300

    todos links externos internalizados

--- torre_de_hanoi/urls.py
+++ torre_de_hanoi/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^', admin.site.urls),
-    url(r'^jogo/', include('jogo.urls')),
+    url(r'^admin/', admin.site.urls),
+    url(r'^', include('jogo.urls')),
 ]
