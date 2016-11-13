cec826f41cb9e842a94eeeadb67d6111d6faa3c8
beatriz-soares <bs.souza1998@gmail.com>
2016-11-13 15:34:14 -0300

    adicionando pagina de instrucoes

--- jogo/views.py
+++ jogo/views.py
@@ -4,6 +4,9 @@ from django.http import HttpResponse
 # Create your views here.from django.http import HttpResponse
 
 def index(request):
-
     return render(request, "jogo/menu.html")
+
+def instrucoes(request):
+    return render(request, "jogo/instrucoes.html")
+
 
