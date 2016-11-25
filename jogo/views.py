a7923458c62f317f551e5869a787f87306097052
Diego Lima <ddddiegolima@gmail.com>
2016-11-24 22:30:19 -0300

    Problema de sessionamento! jogador da vez!

--- jogo/views.py
+++ jogo/views.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 
 # saida
-# Precisamos deixar a sessão zerada na saída!!.
+# Precisamos deixar a sessao zerada na saída!!.
 # if 'qtd' in request.session:
 #     del request.session['qtd']
 # if 'jogador1' in request.session:
@@ -43,21 +43,26 @@ def instrucoes(request):
     return render(request, "jogo/instrucoes.html")
 
 def nomear_jogadores(request):
-    # Guarda-se na sessão a quantidade de jogos que ainda faltam. Serve para
+    # Guarda-se na sessao a quantidade de jogos que ainda faltam. Serve para
     # dizer se ainda falta algum jogador participar.
     request.session['jogos_pendentes'] = 0
-
-    # Salva os nomes dos jogadores na sessão
+    print '*********************'
+    print 'Jogadores nomeados:'
+    # Salva os nomes dos jogadores na sessao
     if request.method == 'POST':
         if 'nome_jogador1' in request.POST:
             request.session['nome_jogador1'] = request.POST['nome_jogador1']
             request.session['jogos_pendentes'] += 1
+            print request.session['nome_jogador1']
         if 'nome_jogador2' in request.POST:
             request.session['nome_jogador2'] = request.POST['nome_jogador2']
             request.session['jogos_pendentes'] += 1
+            print request.session['nome_jogador2']
 
         request.session['jogador_da_vez'] = request.POST['nome_jogador1']
 
+        print "Serão jogados ", request.session['jogos_pendentes'],' jogos'
+        print "Jogador da vez será ", request.session['jogador_da_vez']
         return HttpResponseRedirect(reverse('jogo'))
 
     range_nomes = range(request.session['numero_jogadores'])
@@ -68,20 +73,24 @@ def jogo(request):
     return render(request, "jogo/jogo.html", locals())
 
 def transicao(request, pontos):
-    # Atribui a pontuação ao jogador da vez na sessão e muda o jogador da vez
-    if request.session['jogador_da_vez'] == request.session['nome_jogador1']:
-        request.session['pontos_jogador1'] = pontos
-        if 'nome_jogador2' in request.session:
-            request.session['jogador_da_vez'] == request.session['nome_jogador2']
+    # Atribui a pontuação ao jogador da vez na sessao e muda o jogador da vez
+    if 'jogador_da_vez' in request.session:
+        if request.session['jogador_da_vez'] == request.session['nome_jogador1']:
+            request.session['pontos_jogador1'] = pontos
+            if 'nome_jogador2' in request.session:
+                request.session['jogador_da_vez'] = request.session['nome_jogador2']
+        else:
+            request.session['pontos_jogador2'] = pontos
+            request.session['jogador_da_vez'] = request.session['nome_jogador1']
+
+        # Atualiza a quantidade de jogos que ainda serao jogados.
+        request.session['jogos_pendentes'] -= 1
+
+
+        # Decide se manda pra tela de jogo de novo ou se manda pra tela de resultado
+        # final.
+        if request.session['jogos_pendentes'] > 0:
+            return render(request, "jogo/jogo.html", locals())
+        return render(request, "jogo/transicao.html", locals())
     else:
-        request.session['pontos_jogador2'] = pontos
-        request.session['jogador_da_vez'] == request.session['nome_jogador1']
-
-    # Atualiza a quantidade de jogos que ainda serao jogados.
-    request.session['jogos_pendentes'] -= 1
-
-    # Decide se manda pra tela de jogo de novo ou se manda pra tela de resultado
-    # final.
-    if request.session['jogos_pendentes'] > 0:
-        return render(request, "jogo/jogo.html", locals())
-    return render(request, "jogo/transicao.html", locals())
+        return HttpResponse("Nao sei quem joga agora!")
